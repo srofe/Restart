b10c143d8713add9e91601e5512a77f2e172df8a
Simon Rofe <simon.rofe@ieee.org>
2021-10-28 11:18:49 +1000

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Simon Rofe on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
