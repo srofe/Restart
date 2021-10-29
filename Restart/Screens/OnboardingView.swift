3f3c9c1fde0526531805977776a1500f16c48dec
Simon Rofe <simon.rofe@ieee.org>
2021-10-29 17:20:14 +1000

    Add heading text to view

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -15,12 +15,21 @@ struct OnboardingView: View {
             Color("ColourBlue")
                 .ignoresSafeArea(.all, edges: .all)
             VStack {
-                Text("Onboarding")
-                    .font(.largeTitle)
-                Button(action: {
-                    isOnboardingViewActive = false
-                }) {
-                    Text("Start")
+                Spacer()
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    Text("""
+                        It's not how much we give but
+                        how much love we put into giving.
+                        """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
                 }
             }
         }
