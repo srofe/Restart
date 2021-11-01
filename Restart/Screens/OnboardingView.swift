d1032eaa7d22d4724d04375a2cc07c9314a04486
Simon Rofe <simon.rofe@ieee.org>
2021-11-02 07:31:00 +1000

    Refactor to extract view sections

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -15,74 +15,88 @@ struct OnboardingView: View {
             Color("ColourBlue")
                 .ignoresSafeArea(.all, edges: .all)
             VStack {
-                Spacer()
-                VStack(spacing: 0) {
-                    Text("Share.")
-                        .font(.system(size: 60))
-                        .fontWeight(.heavy)
-                        .foregroundColor(.white)
-                    Text("""
+                header
+                centre
+                footer
+            }
+        }
+    }
+
+    fileprivate var header: some View {
+        Group {
+            Spacer()
+            VStack(spacing: 0) {
+                Text("Share.")
+                    .font(.system(size: 60))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                Text("""
                         It's not how much we give but
                         how much love we put into giving.
                         """)
-                        .font(.title3)
-                        .fontWeight(.light)
-                        .foregroundColor(.white)
-                        .multilineTextAlignment(.center)
-                        .padding(.horizontal, 10)
-                }
-                ZStack {
-                    ZStack {
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 40)
-                            .frame(width: 260, height: 260, alignment: .center)
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 80)
-                            .frame(width: 260, height: 260, alignment: .center)
-                        Image("character-1")
-                            .resizable()
-                            .scaledToFit()
-                    }
-                }
-                ZStack {
-                    Capsule()
-                        .fill(Color.white.opacity(0.2))
-                    Capsule()
-                        .fill(Color.white.opacity(0.2))
-                        .padding(8)
-                    Text("Get Started")
-                        .font(.system(.title3, design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .offset(x: 20)
-                    HStack {
-                        Capsule()
-                            .fill(Color("ColourRed"))
-                            .frame(width: 80)
-                            Spacer()
-                    }
-                    HStack {
-                        ZStack {
-                            Circle()
-                                .fill(Color("ColourRed"))
-                            Circle()
-                                .fill(.black.opacity(0.15))
-                                .padding(8)
-                            Image(systemName: "chevron.right.2")
-                                .font(.system(size: 24, weight: .bold))
-                        }
-                        .foregroundColor(.white)
-                        .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
-                        Spacer()
-                    }
-                }
-                .frame(height: 80, alignment: .center)
-                .padding()
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
             }
         }
+    }
+
+    fileprivate var centre: some View {
+        ZStack {
+            ZStack {
+                Circle()
+                    .stroke(.white.opacity(0.2), lineWidth: 40)
+                    .frame(width: 260, height: 260, alignment: .center)
+                Circle()
+                    .stroke(.white.opacity(0.2), lineWidth: 80)
+                    .frame(width: 260, height: 260, alignment: .center)
+                Image("character-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+    }
+
+    private var footer: some View {
+        ZStack {
+            Capsule()
+                .fill(Color.white.opacity(0.2))
+            Capsule()
+                .fill(Color.white.opacity(0.2))
+                .padding(8)
+            Text("Get Started")
+                .font(.system(.title3, design: .rounded))
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .offset(x: 20)
+            HStack {
+                Capsule()
+                    .fill(Color("ColourRed"))
+                    .frame(width: 80)
+                Spacer()
+            }
+            HStack {
+                ZStack {
+                    Circle()
+                        .fill(Color("ColourRed"))
+                    Circle()
+                        .fill(.black.opacity(0.15))
+                        .padding(8)
+                    Image(systemName: "chevron.right.2")
+                        .font(.system(size: 24, weight: .bold))
+                }
+                .foregroundColor(.white)
+                .frame(width: 80, height: 80, alignment: .center)
+                .onTapGesture {
+                    isOnboardingViewActive = false
+                }
+                Spacer()
+            }
+        }
+        .frame(height: 80, alignment: .center)
+        .padding()
     }
 }
 
